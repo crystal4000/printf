1c3f7f69dcef8090262e7b0421de84444af7287a
Nefertiti <fredrickamanda3@gmail.com>
2023-05-31 13:06:47 -0700

    Second Task Corrections

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdarg.h>
+#include <limits.h>
 #include "main.h"
 
 int handle_conversion_specifier(char specifier, va_list args);
@@ -7,15 +8,13 @@ int handle_conversion_specifier(char specifier, va_list args);
 /**
  * _printf - Custom printf function
  * @format: Format string
- *i
+ *
  * Return: Number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-	int num, num_digits, temp, i;
-	char buffer[12];
 
 	if (format == NULL)
 		return (-1);
@@ -35,6 +34,7 @@ int _printf(const char *format, ...)
 		}
 		format++;
 	}
+
 	va_end(args);
 	return (count);
 }
@@ -49,19 +49,19 @@ int _printf(const char *format, ...)
 int handle_conversion_specifier(char specifier, va_list args)
 {
 	int count = 0;
-	char ch;
-	char *str;
-	char num_str[12];
-	int len, num, num_digits, temp;
 
 	switch (specifier)
 	{
 		case 'c':
-			ch = (char)va_arg(args, int);
+		{
+			char ch = (char)va_arg(args, int);
+
 			count += write(1, &ch, 1);
 			break;
+		}
 		case 's':
-			str = va_arg(args, char *);
+		{
+			char *str = va_arg(args, char *);
 
 			if (str == NULL)
 			{
@@ -76,19 +76,60 @@ int handle_conversion_specifier(char specifier, va_list args)
 				}
 			}
 			break;
+		}
 		case 'd':
 		case 'i':
-			num = va_arg(args, int);
-			len = sprintf(num_str, "%d", num);
-			count += write(1, num_str, len);
+		{
+			int num = va_arg(args, int);
+			int num_digits = 0;
+			int temp = num;
+			char buffer[12];
+			int i;
+
+			if (num == INT_MIN)
+			{
+				count += write(1, "-2147483648", 11);
+			}
+			else
+			{
+				if (num == 0)
+				{
+					count += write(1, "0", 1);
+					break;
+				}
+				if (num < 0)
+				{
+					count += write(1, "-", 1);
+					num = -num;
+				}
+				while (temp != 0)
+				{
+					temp /= 10;
+					num_digits++;
+				}
+				for (i = num_digits - 1; i >= 0; i--)
+				{
+					buffer[i] = '0' + (num % 10);
+					num /= 10;
+				}
+				buffer[num_digits] = '\0';
+				count += write(1, buffer, num_digits);
+			}
 			break;
+		}
 		case '%':
+		{
 			count += write(1, "%", 1);
 			break;
+		}
 		default:
+		{
 			count += write(1, "%d", 1);
 			count += write(1, &specifier, 1);
 			break;
+		}
 	}
+
 	return (count);
 }
+
